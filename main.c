c18ecbdac3116ad95d1ad5678a2626335477778c
Mohamed Elfeki <mohamed.elfeki00@eng-st.cu.edu.eg>
2021-04-17 20:21:53 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student{
+char name[30];
+int id;
+int day;
+int month;
+int year;
+int score;
+};
+
+
+
+int main()
+{
+    printf("welcome");
+
+    return 0;
+}
